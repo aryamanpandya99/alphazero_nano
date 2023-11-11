5719c0d7897680328c205feb61c55b5eae6145a2
aryamanpandya99 <aryaman.pandya@tufts.edu>
2023-11-11 17:34:28 -0500

    modified:   agent.py

--- agent.py
+++ agent.py
@@ -1,37 +1,45 @@
 """
 Author: Aryaman Pandya
-File contents: Implementation of our AlphaZero Nano agent. This file contains the 
-training code and supporting functions required to enable training through self play. 
+File contents: Implementation of our AlphaZero Nano agent.
+This file contains the training code and supporting functions
+required to enable training through self play.
 """
-import numpy as np
 import sys
+
+import numpy as np
 
 sys.path.append("Othello")
 
+from othello_game import OthelloGame
+
 from mcts import apv_mcts
 from models import OthelloNN
-from othello_game import OthelloGame
+from Game import Game
 
 
 class AlphaZeroNano:
     """
-    Class implementation of AlphaZero agent. 
+    Class implementation of AlphaZero agent.
     """
 
-    def __init__(self, neural_network, num_simulations, C) -> None:
+    def __init__(
+            self,
+            neural_network: OthelloNN,
+            num_simulations: int,
+            C: float) -> None:
 
         self.model = neural_network
         self.c_parameter = C
         self.num_simulations = num_simulations
 
     def train(self):
-        ## note to self -> use notes to write out trainer
+        # note to self -> use notes to write out trainer
         pass
 
-    def play_games(self, num_episodes, game):
+    def play_games(self, num_episodes: int, game: Game):
 
         train_episodes = []
-
+        player = 1
         for _ in range(num_episodes):
             game_states = []
             game_state = game.getInitBoard()
@@ -45,9 +53,15 @@ class AlphaZeroNano:
                     c=self.c_parameter,
                 )
                 game_states.append((game, policy))
-                action = np.random.choice(np.arange(game.getActionSize()), p=policy)
+                action = np.random.choice(
+                    np.arange(game.getActionSize()),
+                    p=policy
+                    )
                 # need to figure out how to handle player expression
-                game_state = game_state.getNextState(game_state, player, action)
+                game_state, player = game_state.getNextState(
+                    game_state,
+                    player,
+                    action)
 
             game_result = game_state.getScore()
             for state, policy in game_states:
