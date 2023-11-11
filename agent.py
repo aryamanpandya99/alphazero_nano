418ad6287a8d2351de3cc7af98430f6220c44fce
aryamanpandya99 <aryaman.pandya@tufts.edu>
2023-11-11 18:36:30 -0500

    modified:   agent.py

--- agent.py
+++ agent.py
@@ -15,6 +15,7 @@ from othello_game import OthelloGame
 from mcts import apv_mcts
 from models import OthelloNN
 from Game import Game
+import torch 
 
 
 class AlphaZeroNano:
@@ -33,13 +34,42 @@ class AlphaZeroNano:
         self.num_simulations = num_simulations
 
     def train(self):
-        # note to self -> use notes to write out trainer
+        """
+        
+        ENTER DOCSTRING - main agent trainer 
+        
+        """
         pass
 
-    def play_games(self, num_episodes: int, game: Game):
+    def retrain_nn(self, train_data): 
+        """
+        
+        ENTER DOCSTRING - neural network trainer 
+        
+        """
+        policy_loss_fn = torch.nn.CrossEntropyLoss()
+        value_loss_fn = torch.nn.MSELoss()
+        
+        for state, policy, result in train_data: 
+            x_train = state 
+            policy_train = policy
+            value_train = result
+            
+            policy_pred, value_pred = self.model.predict(state)
 
+            policy_loss = policy_loss_fn(policy_train, policy_pred)
+            value_loss = torch.nn.MSE(value_train, value_loss)
+
+
+    def play_games(self, num_episodes: int, game: Game):
+        """
+        
+        ENTER DOCSTRING - MCTS game player 
+        
+        """
         train_episodes = []
         player = 1
+        
         for _ in range(num_episodes):
             game_states = []
             game_state = game.getInitBoard()
@@ -52,18 +82,18 @@ class AlphaZeroNano:
                     num_iterations=self.num_simulations,
                     c=self.c_parameter,
                 )
-                game_states.append((game, policy))
+                game_states.append((game_state, policy))
                 action = np.random.choice(
                     np.arange(game.getActionSize()),
                     p=policy
                     )
-                # need to figure out how to handle player expression
+                
                 game_state, player = game_state.getNextState(
                     game_state,
                     player,
                     action)
 
-            game_result = game_state.getScore()
+            game_result = game_state.getGameEnded()
             for state, policy in game_states:
                 train_episodes.append((state, policy, game_result))
 
