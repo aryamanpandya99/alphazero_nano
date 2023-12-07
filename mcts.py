54552cf7726c000dd4caedebb73a9fe6b86cc348
aryamanpandya99 <aryaman.pandya@tufts.edu>
2023-12-07 15:35:42 -0800

    refactoring mcts to not rely on individually implemented nodes

--- mcts.py
+++ mcts.py
@@ -76,169 +76,178 @@ class Node:
 
         return np.max(action_uct)
 
+class MCTS:
+    def __init__(self, game) -> None:
+        self.game = game
+        self.total_value_s_a = {}
+        self.q_s_a = {}
+        self.prior_probability = {}
+        self.num_visits_s_a = {}
+        self.num_visits_s = {}
 
-def split_player_boards(board: np.ndarray) -> tuple[np.ndarray, np.ndarray]: 
-    """
-    ENTER docstring
-    """
-    player_a = np.maximum(board, 0)
-    player_b = board.copy()
-    player_b[player_b < 0] = 1
+    def split_player_boards(self, board: np.ndarray) -> tuple[np.ndarray, np.ndarray]: 
+        """
+        ENTER docstring
+        """
+        player_a = np.maximum(board, 0)
+        player_b = board.copy()
+        player_b[player_b < 0] = 1
 
-    return player_a, player_b
-
-
-def update_history_frames(history: np.ndarray, new_frame: np.ndarray, m: int, history_length: int): 
-    """
-    Updates the history of game boards with a new frame.
-
-    Shifts existing frames in history and adds the new frame at the end.
-
-    Args:
-        history (np.ndarray): Game frame history (NxNx(MT+L))
-        new_frame (np.ndarray): 2D array representing new game state.
-        m (int): Number of channels per frame.
-        history_length (int): Number of frames in history.
-
-    Returns:
-        None: Updates 'history' array in place.
-    """
-
-    # still needs work 
-    board_player_1, board_player_2 = split_player_boards(new_frame)
-    history[:m*(history_length-1), :,:] = history[m:, :, :]
-    new_frames = np.stack([board_player_1, board_player_2], axis=0)
-    history[m*(history_length-1):,:,:] = new_frames.reshape(history.shape[0], history.shape[1], m)
+        return player_a, player_b
 
 
-def add_player_information(board_tensor: np.ndarray, current_player: int):
-    """
-    Adds a feature plane indicating the current player.
+    def update_history_frames(self, history: np.ndarray, new_frame: np.ndarray, m: int, history_length: int): 
+        """
+        Updates the history of game boards with a new frame.
 
-    Args:
-        board_tensor (np.ndarray): The tensor representing the game state.
-        current_player (int): The current player (e.g., 0 or 1).
+        Shifts existing frames in history and adds the new frame at the end.
 
-    Returns:
-        np.ndarray: Updated board tensor with the player information added.
-    """
-    # Assuming the last channel is for the current player information
-    player_plane = np.full((board_tensor.shape[0], board_tensor.shape[1]), current_player)
-    board_tensor[:, :, -1] = player_plane
-    return board_tensor
+        Args:
+            history (np.ndarray): Game frame history (NxNx(MT+L))
+            new_frame (np.ndarray): 2D array representing new game state.
+            m (int): Number of channels per frame.
+            history_length (int): Number of frames in history.
 
-def no_history_model_input(board_arr: np.ndarray, current_player: int) -> np.ndarray: 
-    """
-    Alternative model input generator. In this function, we take in the game
-    board and player and return a stack of boards containing one board per player
-    and one board containing current player info. This is an alternative to the method
-    in the paper where we store some T length running memory.
+        Returns:
+            None: Updates 'history' array in place.
+        """
 
-    Args: 
-        board_arr
-        current_player
-    Returns: 
-        model_input_board
-    """
-    board_player_1, board_player_2 = split_player_boards(board_arr)
-    stacked_board = np.stack((board_player_1, board_player_2), axis = 0)
-    if current_player == 1: 
-        player_board = np.ones(board_arr.shape)
-    else: 
-        player_board = np.zeros(board_arr.shape)
-    model_input_board = np.concatenate((stacked_board, player_board[np.newaxis,...]), axis = 0)
-    return model_input_board
+        # still needs work 
+        board_player_1, board_player_2 = self.split_player_boards(new_frame)
+        history[:m*(history_length-1), :,:] = history[m:, :, :]
+        new_frames = np.stack([board_player_1, board_player_2], axis=0)
+        history[m*(history_length-1):,:,:] = new_frames.reshape(history.shape[0], history.shape[1], m)
 
-@torch.no_grad()
-def apv_mcts(
-        game: Game,
-        root_state,
-        model: torch.nn.Module(),
-        num_iterations: int,
-        device,
-        c: float, 
-        temp = 1):
-    """
-    Implementation of the APV-MCTS variant used in the AlphaZero algorithm.
 
-    This algorithm differs from traditional MCTS in that the simulation from
-    leaf node to terminal state is replaced by evaluation by a neural network.
-    This way, instead of having to play out different scenarios and
-    backpropagate received reward, we can just estimate it using a nn and
-    backpropagate that estimated value.
+    def add_player_information(self, board_tensor: np.ndarray, current_player: int):
+        """
+        Adds a feature plane indicating the current player.
 
-    """
-    n, _ = game.getBoardSize()
-    player = 1  # assumption across this system is we're going to start simulations w/ player 1
-    root_node = Node(root_state, game.getActionSize())
-    input_array = np.zeros((3, 8, 8))
-    for _ in range(num_iterations):
-        node = root_node
-        # The purpose of this loop is to get us from our current node to a
-        # terminal node or a leaf node so that we can either end the game
-        # or continue to expand
-        path = []
-        count = 0
-        while (len(node.children.keys()) > 0) and not game.getGameEnded(node.state, player=player):
-            count+=1
-            possible_actions = game.getValidMoves(node.state, player=player)
-            if len(possible_actions) > 0:
-                action = node.select_action(
-                    game=game, valid_moves=possible_actions, c=c
-                )
-                next_state, player = game.getNextState(
-                    board=node.state,
-                    player=player,
-                    action=action)
-                node = Node(next_state, game.getActionSize())
+        Args:
+            board_tensor (np.ndarray): The tensor representing the game state.
+            current_player (int): The current player (e.g., 0 or 1).
 
-            else:
-                logging.info(
-                    "Terminal state: no possible actions from %s", (node.state)
-                )
-                break
-            input_array = no_history_model_input(board_arr=node.state, 
-                                                 current_player=player)
-            path.append((input_array, node, action))
+        Returns:
+            np.ndarray: Updated board tensor with the player information added.
+        """
+        # Assuming the last channel is for the current player information
+        player_plane = np.full((board_tensor.shape[0], board_tensor.shape[1]), current_player)
+        board_tensor[:, :, -1] = player_plane
+        return board_tensor
 
-        # expansion phase
-        # for our leaf node, expand by adding possible children
-        # from that game state to node.children
-        input_tensor = torch.tensor(input_array, dtype=torch.float32).to(device).unsqueeze(0)
-        if not game.getGameEnded(node.state, player=player):
-            cannonical_board = game.getCanonicalForm(node.state, player=player)
-            policy, _  = model(input_tensor)
-            policy = policy.cpu().detach().numpy()
-            possible_actions = game.getValidMoves(node.state, player=player)
-            policy *= possible_actions
+    def no_history_model_input(self, board_arr: np.ndarray, current_player: int) -> np.ndarray: 
+        """
+        Alternative model input generator. In this function, we take in the game
+        board and player and return a stack of boards containing one board per player
+        and one board containing current player info. This is an alternative to the method
+        in the paper where we store some T length running memory.
 
-            for action_idx, probability in enumerate(policy):
-                if probability > 0:
-                    next_state = game.getNextState(
-                        action=action_idx,
-                        board=cannonical_board,
-                        player=player
+        Args: 
+            board_arr
+            current_player
+        Returns: 
+            model_input_board
+        """
+        board_player_1, board_player_2 = self.split_player_boards(board_arr)
+        stacked_board = np.stack((board_player_1, board_player_2), axis = 0)
+        if current_player == 1: 
+            player_board = np.ones(board_arr.shape)
+        else: 
+            player_board = np.zeros(board_arr.shape)
+        model_input_board = np.concatenate((stacked_board, player_board[np.newaxis,...]), axis = 0)
+        return model_input_board
+
+    @torch.no_grad()
+    def apv_mcts(
+            self,
+            game: Game,
+            root_state,
+            model: torch.nn.Module(),
+            num_iterations: int,
+            device,
+            c: float,
+            temp = 1):
+        """
+        Implementation of the APV-MCTS variant used in the AlphaZero algorithm.
+
+        This algorithm differs from traditional MCTS in that the simulation from
+        leaf node to terminal state is replaced by evaluation by a neural network.
+        This way, instead of having to play out different scenarios and
+        backpropagate received reward, we can just estimate it using a nn and
+        backpropagate that estimated value.
+
+        """
+        player = 1  # assumption across this system is we're going to start simulations w/ player 1
+        state = root_state
+        state_string = game.stringRepresentation(state)
+        input_array = np.zeros((3, 8, 8))
+        for _ in range(num_iterations):
+            # The purpose of this loop is to get us from our current node to a
+            # terminal node or a leaf node so that we can either end the game
+            # or continue to expand
+            path = []
+            while (len(self.prior_probability[state_string]) > 0) and not game.getGameEnded(state, player=player):
+                possible_actions = game.getValidMoves(state, player=player)
+                if len(possible_actions) > 0:
+                    action = node.select_action(
+                        game=game, valid_moves=possible_actions, c=c
                     )
-                    child = Node(next_state, game.getActionSize())
-                    child.prior_probability = probability
-                    node.children[action_idx] = child
+                    next_state, player = game.getNextState(
+                        board=state,
+                        player=player,
+                        action=action)
+                    node = Node(next_state, game.getActionSize())
 
-        if len(path) > 0:
-            _, value = model(input_tensor)
+                else:
+                    logging.info(
+                        "Terminal state: no possible actions from %s", (state)
+                    )
+                    break
+                input_array = self.no_history_model_input(board_arr=state,
+                                                    current_player=player)
+                path.append((input_array, state, action))
+                state = next_state
+                state_string = game.stringRepresentation(state)
 
-        # backpropagation phase
-        for _, node, action in reversed(path):
-            node.num_visits_s += 1
-            node.num_visits_s_a[action] += 1
-            node.total_value_s_a[action] += value
-            node.q_s_a[action] = (
-                node.total_value_s_a[action] / node.num_visits_s_a[action]
-            )
-            value = -value  # reverse value we alternates between players
+            # expansion phase
+            # for our leaf node, expand by adding possible children
+            # from that game state to node.children
+            input_tensor = torch.tensor(input_array, dtype=torch.float32).to(device).unsqueeze(0)
+            if not game.getGameEnded(state, player=player) and state_string not in self.prior_probability:
+                cannonical_board = game.getCanonicalForm(state, player=player)
+                policy, _  = model(input_tensor)
+                policy = policy.cpu().detach().numpy()
+                possible_actions = game.getValidMoves(state, player=player)
+                policy *= possible_actions
+                if np.sum(self.prior_probability[state_string]) > 0: 
+                    self.prior_probability[state_string] /= np.sum(self.prior_probability[state_string])
+                for action_idx, probability in enumerate(policy):
+                    if probability > 0:
+                        next_state = game.getNextState(
+                            action=action_idx,
+                            board=cannonical_board,
+                            player=player
+                        )
+                        child = Node(next_state, game.getActionSize())
+                        child.prior_probability = probability
+                        node.children[action_idx] = child
 
-    root = path[0][1]
-    visits = [x ** 1 / temp for x in root.num_visits_s_a]
-    sum_visits = float(sum(visits))
-    pi = [x / sum_visits for x in visits]
+            if len(path) > 0:
+                _, value = model(input_tensor)
 
-    return pi
+            # backpropagation phase
+            for _, node, action in reversed(path):
+                node.num_visits_s += 1
+                node.num_visits_s_a[action] += 1
+                node.total_value_s_a[action] += value
+                node.q_s_a[action] = (
+                    node.total_value_s_a[action] / node.num_visits_s_a[action]
+                )
+                value = -value  # reverse value we alternates between players
+
+        root = path[0][1]
+        visits = [x ** 1 / temp for x in root.num_visits_s_a]
+        sum_visits = float(sum(visits))
+        pi = [x / sum_visits for x in visits]
+
+        return pi
